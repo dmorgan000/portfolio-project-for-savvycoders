63d9c4fbc5129230b354934c4afbc5ff244564b8
Debra Handley <dmorgan000@yahoo.com>
2016-11-21 16:46:12 -0600

    Updated code to match Alexs 11/19 class

--- projects/loops/site.js
+++ projects/loops/site.js
@@ -4,57 +4,122 @@ var counter = 100;
 
 while( counter > 0 ){
   if( counter % 15 === 0 ){
-    outputList += "<li>( FizzBuzz</li>";
+      // console.log( "FizzBuzz" );
+    outputList += "<li>FizzBuzz</li>";
   }
   else if( counter % 3 === 0 ){
-    outputList += "<li>( Fizz</li>";
+      // console.log( "Fizz" );
+    outputList += "<li>Fizz</li>";
   }
-  else if(counter % 5 === 0 ){
-    outputList += "<li>( Buzz</li>";
+  else if( counter % 5 === 0 ){
+      // console.log( "Buzz" );
+    outputList += "<li>Buzz</li>";
   }
   else{
-    outputList += "<li>" + counter +"</li>";
+    console.log( counter );
+    outputList += "<li>" + counter + "</li>";
   }
 
   counter--;
 }
 
+// output.innerHTML = "<h3>Hello, I came from site.js</h3>";
+
 outputList += "</ul>";
 
 output.innerHTML = outputList;
 
+
 // ROCK PAPER SCISSORS
-var runRound = function (){
-    var userChoice = prompt( "What would you like to play? Rock, Paper, or Scissor, pelase" );
-    var rng = Math.random();
-    var computerChoice = "Scissors";
+var getComputerChoice = function(){
+  var computerChoice = "scissors";
+  var rng = Math.random();
+
+  if( rng < 0.33 ){
+    computerChoice = "rock";
+  }
+  else if( rng < 0.66 ){
+      computerChoice = "paper";
+  }
+
+  return computerChoice;
+}
+
+var getWinner = function( computerChoice, userChoice ){
     var winner = "computer";
+    var lowerCaseUserChoice = userChoice.toLowerCase();
 
-    if( rng < 0.33 ){
-      computerChoice = "Rock";
-      }
-    else if( rng < 0.66){
-      computerChoice = "Paper";
-    }
-
-    if( computerChoice === "Scissors" && userChoice === "Rock" ){
+    if( computerChoice === "scissors" && lowerCaseUserChoice === "rock" ){
       winner = "user";
     }
-    else if ( computerChoice === "Paper" && userChoice === "Scissors" ){
+    else if ( computerChoice === "paper" && lowerCaseUserChoice === "scissors" ){
         winner = "user";
     }
-    else if ( computerChoice === "Rock" && userChoice === "Paper" ){
+    else if ( computerChoice === "rock" && lowerCaseUserChoice === "paper" ){
         winner = "user";
     }
+    return winner;
+}
 
-    if(computerChoice === userChoice ){
-      alert( "There was a tie!" );
+var validateUserChoice = function( userChoice ){
+  var lowerCaseUserChoice = userChoice.toLowerCase();
+
+  return lowerCaseUserChoice === "rock" ||
+  lowerCaseUserChoice === "paper" ||
+  lowerCaseUserChoice === "scissors";
+}
+
+
+var runRound = function (){
+    var userChoice = prompt( "What would you like to play? Rock, Paper, or Scissor, please" );
+    var computerChoice = getComputerChoice();
+    var winner = getWinner( computerChoice, userChoice );
+
+    if( !validateUserChoice( userChoice ) ){
+      alert("Please input a valid command!" );
 
       runRound();
     }
-    else{
-      alert( "The winner was " + winner );
-    }
+      else{
+        if( computerChoice === userChoice ){
+          alert( "There was a tie!");
+
+          runRound();
+        }
+        else{
+          alert( "The winner was " + winner );
+        }
+      }
+                // 11/19/16 COMMNENTED OUT MY CODE AND REPLACED WITH ALEX'S CODE
+                //     var rng = Math.random();
+                    // var computerChoice = "Scissors";
+                //     var winner = "computer";
+                //
+                //     if( rng < 0.33 ){
+                //       computerChoice = "Rock";
+                //       }
+                //     else if( rng < 0.66){
+                //       computerChoice = "Paper";
+                //     }
+                //
+                    // if( computerChoice === "Scissors" && userChoice === "Rock" ){
+                    //   winner = "user";
+                    // }
+                    // else if ( computerChoice === "Paper" && userChoice === "Scissors" ){
+                    //     winner = "user";
+                    // }
+                    // else if ( computerChoice === "Rock" && userChoice === "Paper" ){
+                    //     winner = "user";
+                    // }
+
+                    // if(computerChoice === userChoice ){
+                    //   alert( "There was a tie!" );
+                    //
+                    //   runRound();
+                    // }
+                    // else{
+                    //   alert( "The winner was " + winner );
+                    // }
 }
 var roundCounter = 1;
 
